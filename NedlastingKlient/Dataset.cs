01c319be5b70685cddf622fa7113f912682aca73
Veronika Andersen <veronika@arkitektum.no>
2018-02-06 15:33:18 +0100

    Create Dataset class

---
+++ NedlastingKlient/Dataset.cs
@@ -0,0 +1,12 @@
+﻿namespace NedlastingKlient
+{
+    public class Dataset
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Uuid { get; set; }
+        public string FeedUrl { get; set; }
+        public string LastUpdated { get; set; }
+        public string Organization { get; set; }
+    }
+}
