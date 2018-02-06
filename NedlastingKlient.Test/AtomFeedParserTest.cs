0b45965080aeb972693fa10092e7edd8df67db5f
Veronika Andersen <veronika@arkitektum.no>
2018-02-06 15:34:09 +0100

    Add AtomFeedParser

---
+++ NedlastingKlient.Test/AtomFeedParserTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace NedlastingKlient.Test
+{
+    public class AtomFeedParserTest
+    {
+
+        [Fact]
+        public void ShouldParseAtomFeed()
+        {
+            List<Dataset> result = new AtomFeedParser().Parse(File.ReadAllText("Tjenestefeed.xml"));
+            result.Count.Should().Be(3);
+        }
+
+    }
+}
---
+++ NedlastingKlient/AtomFeedParser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace NedlastingKlient
+{
+    public class AtomFeedParser
+    {
+        public List<Dataset> Parse(string xml)
+        {
+            var datasets = new List<Dataset>();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xml);
+
+            string xpath = "//a:feed/a:entry";
+
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
+            nsmgr.AddNamespace("a", "http://www.w3.org/2005/Atom");
+            nsmgr.AddNamespace("inspire_dls", "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0");
+
+            var nodes = xmlDoc.SelectNodes(xpath, nsmgr);
+
+            foreach (XmlNode childrenNode in nodes)
+            {
+                var dataset = new Dataset();
+                dataset.Title = childrenNode.SelectSingleNode("a:title", nsmgr).InnerXml;
+                dataset.Description = childrenNode.SelectSingleNode("a:summary", nsmgr).InnerXml;
+                dataset.FeedUrl = childrenNode.SelectSingleNode("a:link", nsmgr).InnerXml;
+                dataset.LastUpdated = childrenNode.SelectSingleNode("a:updated", nsmgr).InnerXml;
+                dataset.Organization = childrenNode.SelectSingleNode("a:author/a:name", nsmgr).InnerXml;
+                dataset.Uuid = childrenNode.SelectSingleNode("inspire_dls:spatial_dataset_identifier_code", nsmgr).InnerXml;
+
+                datasets.Add(dataset);
+            }
+            return datasets;
+        }
+    }
+}
