39e1819a94c8503cfb9e6c49e4a0eace8d6b4f88
Henning Jensen <henning@arkitektum.no>
2018-02-19 15:07:42 +0100

    Download files specified in json file

---
+++ NedlastingKlient.Konsoll/FileDownloader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NedlastingKlient.Konsoll
+{
+    /// <summary>
+    /// Download file with progress event
+    /// 
+    /// https://stackoverflow.com/a/43169927/725492
+    /// </summary>
+    public class FileDownloader
+    {
+        public delegate void ProgressChangedHandler(long? totalFileSize, long totalBytesDownloaded,
+            double? progressPercentage);
+
+        private static readonly HttpClient Client = new HttpClient();
+
+        public event ProgressChangedHandler ProgressChanged;
+
+        public async Task StartDownload(string downloadUrl, string destinationFilePath)
+        {
+
+            using (var response = await Client.GetAsync(downloadUrl, HttpCompletionOption.ResponseHeadersRead))
+            {
+                await DownloadFileFromHttpResponseMessage(response, destinationFilePath);
+            }
+        }
+
+        private async Task DownloadFileFromHttpResponseMessage(HttpResponseMessage response, string destinationFilePath)
+        {
+            response.EnsureSuccessStatusCode();
+
+            var totalBytes = response.Content.Headers.ContentLength;
+
+            using (var contentStream = await response.Content.ReadAsStreamAsync())
+            {
+                await ProcessContentStream(totalBytes, contentStream, destinationFilePath);
+            }
+        }
+
+        private async Task ProcessContentStream(long? totalDownloadSize, Stream contentStream, string destinationFilePath)
+        {
+            var totalBytesRead = 0L;
+            var readCount = 0L;
+            var buffer = new byte[8192];
+            var isMoreToRead = true;
+
+            using (var fileStream = new FileStream(destinationFilePath, FileMode.Create, FileAccess.Write,
+                FileShare.None, 8192, true))
+            {
+                do
+                {
+                    var bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0)
+                    {
+                        isMoreToRead = false;
+                        TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                        continue;
+                    }
+
+                    await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                    totalBytesRead += bytesRead;
+                    readCount += 1;
+
+                    if (readCount % 100 == 0)
+                        TriggerProgressChanged(totalDownloadSize, totalBytesRead);
+                } while (isMoreToRead);
+            }
+        }
+
+        private void TriggerProgressChanged(long? totalDownloadSize, long totalBytesRead)
+        {
+            if (ProgressChanged == null)
+                return;
+
+            double? progressPercentage = null;
+            if (totalDownloadSize.HasValue)
+                progressPercentage = Math.Round((double) totalBytesRead / totalDownloadSize.Value * 100, 2);
+
+            ProgressChanged(totalDownloadSize, totalBytesRead, progressPercentage);
+        }
+    }
+}
--- NedlastingKlient.Konsoll/Program.cs
+++ NedlastingKlient.Konsoll/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.IO;
 
 namespace NedlastingKlient.Konsoll
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Geonorge Nedlastingklient");
+
+            var datasetService = new DatasetService();
+            var datasetToDownload = datasetService.GetSelectedFiles();
+
+            var downloader = new FileDownloader();
+            foreach (var dataset in datasetToDownload)
+            {
+
+                if (!ShouldDownload(dataset))
+                    continue;
+
+                Console.WriteLine("-------------");
+                Console.WriteLine(dataset.DatasetId + " - " + dataset.Title);
+
+                downloader.ProgressChanged += (totalFileSize, totalBytesDownloaded, progressPercentage) =>
+                {
+                    Console.CursorLeft = 0;
+                    Console.Write($"{progressPercentage}% ({totalBytesDownloaded}/{totalFileSize})");
+                };
+
+                var outputDirectory = new DirectoryInfo(@"c:\dev\tmp\geonorge\" + dataset.DatasetId);
+                if (!outputDirectory.Exists)
+                    outputDirectory.Create();
+
+                var uri = new Uri(dataset.Url);
+                var outputFile = outputDirectory.FullName + Path.DirectorySeparatorChar +
+                                 Path.GetFileName(uri.LocalPath);
+
+                downloader.StartDownload(dataset.Url, outputFile).Wait();
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool ShouldDownload(DatasetFile dataset)
+        {
+            // TODO: implement check of LastUpdated both in JSON-file and in Atom feed
+            return true;
         }
     }
 }
