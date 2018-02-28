25d7691d3cd59ae6bd75d53f0051dda5a3f52320
Henning Jensen <henning@arkitektum.no>
2018-02-28 12:44:51 +0100

    Add username and passord fields to AppSettings

--- NedlastingKlient/AppSettings.cs
+++ NedlastingKlient/AppSettings.cs
@@ -10,5 +10,7 @@
         /// </summary>
         public string DownloadDirectory { get; set; }
         
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
