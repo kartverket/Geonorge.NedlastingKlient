813ea1766ca3dc8e8fd08e02871d7f3451e9c75f
Henning Jensen <henning@arkitektum.no>
2018-03-14 09:45:58 +0100

    Clean up

--- NedlastingKlient.Gui/MainWindow.xaml.cs
+++ NedlastingKlient.Gui/MainWindow.xaml.cs
@@ -5,30 +5,26 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Security.Policy;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
-using Button = System.Windows.Controls.Button;
-using ListBox = System.Windows.Controls.ListBox;
-using MessageBox = System.Windows.MessageBox;
 
 namespace NedlastingKlient.Gui
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
+        private readonly DatasetService _datasetService;
+
+        private List<DatasetFileViewModel> _selectedDatasetFiles;
         //public ICommand ShowProgressDialogCommand { get; }
 
         private List<DatasetFileViewModel> _selectedFiles;
-        private List<DatasetFileViewModel> _selectedDatasetFiles;
-        private List<Dataset> _datasets;
         public bool LoggedIn;
-        private DatasetService _datasetService;
 
         public MainWindow()
         {
@@ -39,9 +35,8 @@ namespace NedlastingKlient.Gui
 
             _datasetService = new DatasetService();
 
-            _datasets = _datasetService.GetDatasets();
-            LbDatasets.ItemsSource = _datasets;
-            CollectionView viewDatasets = (CollectionView) CollectionViewSource.GetDefaultView(LbDatasets.ItemsSource);
+            LbDatasets.ItemsSource = _datasetService.GetDatasets();
+            var viewDatasets = (CollectionView) CollectionViewSource.GetDefaultView(LbDatasets.ItemsSource);
             if (viewDatasets != null) viewDatasets.Filter = UserDatasetFilter;
 
             _selectedFiles = _datasetService.GetSelectedFilesAsViewModel();
@@ -52,20 +47,20 @@ namespace NedlastingKlient.Gui
 
         private bool UserDatasetFilter(object item)
         {
-            if (String.IsNullOrEmpty(SearchDataset.Text))
+            if (string.IsNullOrEmpty(SearchDataset.Text))
                 return true;
-            else
-                return ((item as Dataset).Title.IndexOf(SearchDataset.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
-                        (item as Dataset).Organization.IndexOf(SearchDataset.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+            return (item as Dataset).Title.IndexOf(SearchDataset.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                   (item as Dataset).Organization.IndexOf(SearchDataset.Text, StringComparison.OrdinalIgnoreCase) >= 0;
         }
 
         private bool UserDatasetFileFilter(object item)
         {
-            if (String.IsNullOrEmpty(SearchDatasetFiles.Text))
+            if (string.IsNullOrEmpty(SearchDatasetFiles.Text))
                 return true;
-            else
-                return ((item as DatasetFileViewModel).Title.IndexOf(SearchDatasetFiles.Text, StringComparison.OrdinalIgnoreCase) >= 0 ||
-                        (item as DatasetFileViewModel).Category.IndexOf(SearchDatasetFiles.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+            return (item as DatasetFileViewModel).Title.IndexOf(SearchDatasetFiles.Text,
+                       StringComparison.OrdinalIgnoreCase) >= 0 ||
+                   (item as DatasetFileViewModel).Category.IndexOf(SearchDatasetFiles.Text,
+                       StringComparison.OrdinalIgnoreCase) >= 0;
         }
 
 
@@ -75,18 +70,16 @@ namespace NedlastingKlient.Gui
             {
                 if (listBoxItem.SelectedItems.Count > 0)
                 {
-                    Dataset selectedDataset = (Dataset)listBoxItem.SelectedItems[0];
+                    var selectedDataset = (Dataset) listBoxItem.SelectedItems[0];
                     if (selectedDataset != null)
                     {
                         progressBar.IsIndeterminate = true;
 
                         LbSelectedDatasetFiles.ItemsSource = await Task.Run(() => GetFilesAsync(selectedDataset));
                         progressBar.IsIndeterminate = false;
-                        CollectionView viewDatasetFiles = (CollectionView)CollectionViewSource.GetDefaultView(LbSelectedDatasetFiles.ItemsSource);
-                        if (viewDatasetFiles != null)
-                        {
-                            viewDatasetFiles.Filter = UserDatasetFileFilter;
-                        }
+                        var viewDatasetFiles =
+                            (CollectionView) CollectionViewSource.GetDefaultView(LbSelectedDatasetFiles.ItemsSource);
+                        if (viewDatasetFiles != null) viewDatasetFiles.Filter = UserDatasetFileFilter;
                     }
                 }
 
@@ -97,32 +90,25 @@ namespace NedlastingKlient.Gui
 
         private List<DatasetFileViewModel> GetFilesAsync(Dataset selctedDataset)
         {
-            List<DatasetFileViewModel> selectedDatasetFiles = _datasetService.GetDatasetFiles(selctedDataset);
+            var selectedDatasetFiles = _datasetService.GetDatasetFiles(selctedDataset);
 
-            foreach (DatasetFileViewModel selectedDatasetFile in _selectedFiles)
-            {
-                foreach (var datasetFile in selectedDatasetFiles)
+            foreach (var selectedDatasetFile in _selectedFiles)
+            foreach (var datasetFile in selectedDatasetFiles)
+                if (selectedDatasetFile.Id == datasetFile.Id)
                 {
-                    if (selectedDatasetFile.Id == datasetFile.Id)
-                    {
-                        datasetFile.SelectedForDownload = true;
-                        break;
-                    }
+                    datasetFile.SelectedForDownload = true;
+                    break;
                 }
-            }
 
-            if (selectedDatasetFiles.Count == 0)
-            {
-                MessageBox.Show("Ingen filer for dette datasettet");
-            }
+            if (selectedDatasetFiles.Count == 0) MessageBox.Show("Ingen filer for dette datasettet");
             _selectedDatasetFiles = selectedDatasetFiles;
             return selectedDatasetFiles;
         }
 
         private void AddRemove_OnChecked(object sender, RoutedEventArgs e)
         {
-            ToggleButton btn = (ToggleButton)sender;
-            DatasetFileViewModel datasetFile = (DatasetFileViewModel)btn.DataContext;
+            var btn = (ToggleButton) sender;
+            var datasetFile = (DatasetFileViewModel) btn.DataContext;
 
             if (btn.IsChecked == true)
             {
@@ -172,8 +158,8 @@ namespace NedlastingKlient.Gui
 
         private void RemoveFromDownloadList_Click(object sender, RoutedEventArgs e)
         {
-            Button btn = (Button)sender;
-            DatasetFileViewModel selectedDatasetFile = (DatasetFileViewModel)btn.DataContext;
+            var btn = (Button) sender;
+            var selectedDatasetFile = (DatasetFileViewModel) btn.DataContext;
             UpdateSelectedDatasetFiles(selectedDatasetFile);
 
             _selectedFiles.Remove(selectedDatasetFile);
@@ -181,27 +167,22 @@ namespace NedlastingKlient.Gui
         }
 
 
-
         private void UpdateSelectedDatasetFiles(DatasetFileViewModel selectedDatasetFile)
         {
             if (_selectedDatasetFiles.Any())
-            {
-                foreach (DatasetFileViewModel datasetFile in _selectedDatasetFiles)
-                {
+                foreach (var datasetFile in _selectedDatasetFiles)
                     if (datasetFile.Id == selectedDatasetFile.Id)
                     {
                         datasetFile.SelectedForDownload = false;
                         break;
                     }
-                }
-            }
         }
 
         private void ClosingWindow(object sender, CancelEventArgs e)
         {
             SaveDownloadList();
         }
- 
+
         private void SaveDownloadList()
         {
             _datasetService.WriteToDownloadFile(_selectedFiles);
@@ -212,25 +193,22 @@ namespace NedlastingKlient.Gui
             if (LbSelectedDatasetFiles.Items.IsEmpty) return;
             if (BtnSelectAll.IsChecked == true)
             {
+
                 foreach (DatasetFileViewModel datasetFile in LbSelectedDatasetFiles.Items)
-                {
                     if (!datasetFile.SelectedForDownload)
                     {
                         datasetFile.SelectedForDownload = true;
                         AddToList(datasetFile);
                     }
-                }
             }
             else
             {
-                foreach (DatasetFileViewModel datasetFile in _selectedDatasetFiles)
-                {
+                foreach (var datasetFile in _selectedDatasetFiles)
                     if (datasetFile.SelectedForDownload)
                     {
                         datasetFile.SelectedForDownload = false;
                         RemoveFromList(datasetFile);
                     }
-                }
             }
 
             LbSelectedDatasetFiles.ItemsSource = null;
@@ -241,17 +219,16 @@ namespace NedlastingKlient.Gui
         {
             if (_selectedFiles.Any())
             {
-                MessageBoxResult result = MessageBox.Show("Er du sikker på at du vil slette alle", "Slett alle", MessageBoxButton.YesNo);
+                var result = MessageBox.Show("Er du sikker på at du vil slette alle", "Slett alle",
+                    MessageBoxButton.YesNo);
 
                 if (result == MessageBoxResult.Yes)
                 {
                     _selectedFiles = new List<DatasetFileViewModel>();
-                    foreach (DatasetFileViewModel datasetfile in _selectedDatasetFiles)
-                    {
-                        datasetfile.SelectedForDownload = false;
-                    }
+                    foreach (var datasetfile in _selectedDatasetFiles) datasetfile.SelectedForDownload = false;
                     BindNewList();
                 }
+
                 BtnSelectAll.IsChecked = false;
             }
         }
@@ -260,18 +237,19 @@ namespace NedlastingKlient.Gui
         {
             SaveDownloadList();
 
-            string executingAssemblyDirectory = GetExecutingAssemblyDirectory();
+            var executingAssemblyDirectory = GetExecutingAssemblyDirectory();
 
-            var pathToConsoleApp = Path.Combine(executingAssemblyDirectory, "..", "Console", "NedlastingKlient.Konsoll.exe");
-            
+            var pathToConsoleApp =
+                Path.Combine(executingAssemblyDirectory, "..", "Console", "NedlastingKlient.Konsoll.exe");
+
             Process.Start(pathToConsoleApp);
         }
 
         private static string GetExecutingAssemblyDirectory()
         {
-            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
-            UriBuilder uri = new UriBuilder(codeBase);
-            string path = Uri.UnescapeDataString(uri.Path);
+            var codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            var uri = new UriBuilder(codeBase);
+            var path = Uri.UnescapeDataString(uri.Path);
             return Path.GetDirectoryName(path);
         }
 
@@ -283,26 +261,19 @@ namespace NedlastingKlient.Gui
 
         private void SearchDatasetFiles_OnTextChanged(object sender, TextChangedEventArgs e)
         {
-            if (LbSelectedDatasetFiles != null&& LbSelectedDatasetFiles.ItemsSource != null)
-            {
+            if (LbSelectedDatasetFiles != null && LbSelectedDatasetFiles.ItemsSource != null)
                 CollectionViewSource.GetDefaultView(LbSelectedDatasetFiles.ItemsSource).Refresh();
-            }
         }
 
         private void BtnSettings_OnClick(object sender, RoutedEventArgs e)
         {
-            SettingsDialog loginDialog = new SettingsDialog();
+            var loginDialog = new SettingsDialog();
             loginDialog.ShowDialog();
         }
 
         private void BtnHelp_OnClick(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                Process.Start("https://www.geonorge.no/");
-            }
-            catch { }
+            Process.Start("https://www.geonorge.no/");
         }
     }
-
 }
