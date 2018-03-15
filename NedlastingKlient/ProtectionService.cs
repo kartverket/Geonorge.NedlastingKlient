022ba87beacd882cab4ef526ae1e07294a6648e4
Henning Jensen <henning@arkitektum.no>
2018-03-15 16:22:18 +0100

    Clean up

--- NedlastingKlient/ProtectionService.cs
+++ NedlastingKlient/ProtectionService.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
-using Microsoft.AspNetCore.DataProtection;
+﻿using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NedlastingKlient
 {
     public class ProtectionService
     {
-        private static readonly IDataProtector _protector = AddDataProtectionService();
+        private static readonly IDataProtector Protector = AddDataProtectionService();
 
         public static string CreateProtectedPassword(string unprotectedPassword)
         {
-            return _protector.Protect(unprotectedPassword);
+            return Protector.Protect(unprotectedPassword);
         }
 
         private static IDataProtector AddDataProtectionService()
@@ -21,23 +17,13 @@ namespace NedlastingKlient
             var serviceCollection = new ServiceCollection();
             serviceCollection.AddDataProtection();
             var services = serviceCollection.BuildServiceProvider();
-            IDataProtector protector = services.GetDataProtector("Auth.Download");
+            var protector = services.GetDataProtector("Auth.Download");
             return protector;
         }
 
         public static string GetUnprotectedPassword(string protectedPassword)
         {
-            return string.IsNullOrEmpty(protectedPassword) ? null : _protector.Unprotect(protectedPassword);
-            try
-            {
-                return _protector.Unprotect(protectedPassword);
-            }
-            catch (CryptographicException e)
-            {
-                Console.WriteLine("Data was not encrypted. An error occurred.");
-                Console.WriteLine(e.ToString());
-                return null;
-            }
+            return string.IsNullOrEmpty(protectedPassword) ? null : Protector.Unprotect(protectedPassword);
         }
     }
 }
