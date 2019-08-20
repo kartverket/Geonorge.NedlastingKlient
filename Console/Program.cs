56ecb2bb0140b88fe44641af3efa5e05003b9652
Dag Olav Dahle <dagolav@arkitektum.no>
2019-08-20 11:14:59 +0200

    Null reference check formats

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -85,7 +85,8 @@ namespace Geonorge.Nedlaster
                     var datasetFilesFromFeed = datasetService.GetDatasetFiles(localDataset);
 
                     var filterDatasetFromFeed = datasetFilesFromFeed.Where(p => localDataset.Projections.Where(s => s.Selected == false).All(p2 => p2.Epsg != p.Projection)).ToList();
-                    filterDatasetFromFeed = filterDatasetFromFeed.Where(p => localDataset.Formats.Where(s => s.Selected == false).All(f2 => f2.Code != p.Format)).ToList();
+                    if(localDataset.Formats != null)
+                        filterDatasetFromFeed = filterDatasetFromFeed.Where(p => localDataset.Formats.Where(s => s.Selected == false).All(f2 => f2.Code != p.Format)).ToList();
                     if (localDataset.AutoDeleteFiles)
                     {
                         Log.Debug("Delete files");
